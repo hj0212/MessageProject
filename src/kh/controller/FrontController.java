eff5cc066caaa5b15d499328d400fc49bfec3e7b
winhae <winhae@gmail.com>
2018-06-14 11:50:13 +0900

    FrontController  생성
    input.do 추가되었습니다.

--- src/kh/controller/FrontController.java
+++ src/kh/controller/FrontController.java
@@ -1,38 +1,48 @@
 package kh.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class FrontController
- */
-@WebServlet("/FrontController")
+@WebServlet("*.do")
 public class FrontController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FrontController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String requestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String command = requestURI.substring(contextPath.length());
+		
+		request.setCharacterEncoding("utf8");		
+		boolean isRedirect=true;
+		String dst = null;
+		
+		MessageDAO mdao = new MessageDAO();
+		
+		if(command.equals("/input.do")) {
+			String name = request.getParameter("name");
+			String message = request.getParameter("message");		
+			MessageDTO dto = new MessageDTO(name, message);
+			
+			int result = mdao.insertMessage(dto);
+			isRedirect = false;
+			dst = "inputview.jsp";		
+		}
+		
+		if(isRedirect) {
+			response.sendRedirect(dst);
+		}else {
+			RequestDispatcher rd = request.getRequestDispatcher(dst);
+			rd.forward(request, response);
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
